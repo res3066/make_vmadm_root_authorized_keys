f281ff3fac985a9c6581079d5468583af051e613
Rob Seastrom <rs@seastrom.com>
2019-05-05 20:08:15 +0000

    make a traditional authorized_keys file too in case that is useful

--- files/mk_root_authorized_keys.sh
+++ files/mk_root_authorized_keys.sh
@@ -7,3 +7,11 @@ do
 done
 ) | awk '{ printf "root_authorized_keys: \"%s\"\n", $0 }' > root_authorized_keys.yml
 
+(
+for i in `ls *.pub`
+do
+  cat $i | awk '{ print $0 }'
+done
+) > authorized_keys
+
+
